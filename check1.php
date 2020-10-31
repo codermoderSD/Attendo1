b220f50dbe2198b705e7c5fb612803bacc194794
Shubham Dalvi <67367732+codermoderSD@users.noreply.github.com>
2020-10-31 19:53:36 +0530

    Update check1.php
    second commit

--- check1.php
+++ check1.php
@@ -1,42 +1,62 @@
 <html>
-<head><title>Attendo|<?php echo $_GET["email"];?></title>
-<link href="icon.png" rel="icon"/></head>
-<body style="background-color: #87cefa;">
-<div>
-<header style="height:120px; background-color: #123255;">
-<h4 style="padding-left:20px; display:inline-block; color:rgb(243, 152, 119)";>Welcome <?php echo $_GET["email"];?></h4>
-<a href="front.html"><h4 style=" display:inline-block; margin: 0px 9px;
-            color: rgb(243, 152, 119);
-            background-color: #123255;
-            padding: 8px 20px;
-            border: 2px solid rgb(243, 152, 119);
-            border-radius: 50px;
-            font-size: 13px;
-            cursor: pointer;
-            outline: none;
-            width: 50px;">Log Out</h4></a>
-<a href="front.html">
-<img style="padding-right:20px; float:right; width: 120px;" src="logo.jpeg"><a href="front.html"></a></img>
-</header>
-</div>
-<?php 
-$username = "Attendo"; 
-$password = "teamattendo@vit567"; 
+
+<head>
+    <title>Attendo|<?php echo $_GET["email"];?></title>
+    <link href="icon.png" rel="icon" />
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<body>
+    <div>
+        <header style="background-color: #123255; padding: 1rem; display:flex; justify-content: space-between;">
+            <div style="display:flex;">    
+                <h4 style="color:rgb(248, 205, 191); " ;>Welcome
+                    <?php echo $_GET["email"];?></h4>
+            </div>
+            <a href="mainPage.html" style="display:flex; text-decoration: none;">
+                <h4 style=" display:inline-block; margin: 0px 1rem;
+                            color: rgb(248, 205, 191);
+                            background-color: #123255;
+                            padding: .3rem;
+                            text-align: center;
+                            border: 2px solid rgb(248, 205, 191);
+                            border-radius: 50px;
+                            font-size: 1rem;
+                            cursor: pointer;
+                            outline: none;
+                            width: 6.5rem;">
+                            Log Out</h4>
+            </a>
+        </header>
+        <footer style="position:absolute; right: 1rem; bottom: 1rem;">
+                <img style="width: 8rem; opacity:0.6" src="logo.jpeg"><a
+                    href="mainPage.html"></a></img>
+        </footer>
+    </div>
+    <?php 
+$username = "root"; 
+$password = ""; 
 $database = "csv_db 6"; 
 $mysqli = new mysqli("localhost", $username, $password, $database);
 $x=$_GET['email'];
 $query = "SELECT * FROM attendance WHERE name='$x' ORDER BY Date DESC";
  
  
-echo '<br><table style="margin-left: auto; 
-margin-right: auto; width:50%; color:#123255; font-weight:bold; 
-font-size:20px; border-collapse: collapse; border:2px solid rgb(255, 100, 82)" 
-border="3" cellspacing="1" cellpadding="5"> 
-      <tr>
-          <td style="color:white;"> <font face="Times">Name</font> </td> 
-          <td style="color:white"> <font face="Times">Date</font> </td> 
-          <td style="color:white"> <font face="Times">Time</font> </td>
-      </tr>';
+echo '<br><table table-dark style="margin-left: auto; 
+margin-right: auto; width:60%; text-align: center; color:#0a1b2e; font-weight:bold; 
+font-size:20px; border-collapse: collapse; background-color: #a7c4e4; border:2px solid #112336" border="3"
+        cellspacing="1" cellpadding="5">
+        <tr style="background-color: #112336;">
+            <td style="color:white">
+                <font face="Times">Name</font>
+            </td>
+            <td style="color:white">
+                <font face="Times">Date</font>
+            </td>
+            <td style="color:white">
+                <font face="Times">Time</font>
+            </td>
+        </tr>';
  
 if ($result = $mysqli->query($query)) {
     while ($row = $result->fetch_assoc()) {
@@ -53,5 +73,7 @@ if ($result = $mysqli->query($query)) {
     $result->free();
 } 
 ?>
+
 </body>
+
 </html>
